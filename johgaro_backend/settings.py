438b37b823ecbbad6c99371f91b320fec3525242
254jack <taixonj@gmail.com>
2025-09-20 19:20:50 +0300

    modified the secret key in the settings file

--- johgaro_backend/settings.py
+++ johgaro_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z0*9%29a8-v@=l=*2fyxq!+&bbandv)!k_!%kqet!%*_pb(i0+'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
